6e89cc2ac1ad43521a72d2715b45a8f85816a7af
Piyush Gahlawat <piyush.gahlawat.blazing@gmail.com>
2026-05-20 17:43:41 +0530

    optimized exception handler tests

--- src/test/java/com/example/employeeapi/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/com/example/employeeapi/exception/GlobalExceptionHandlerTest.java
@@ -3,6 +3,7 @@ package com.example.employeeapi.exception;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-
+@AutoConfigureMockMvc
 @DisplayName("Global Exception Handler Tests")
 class GlobalExceptionHandlerTest {
 
